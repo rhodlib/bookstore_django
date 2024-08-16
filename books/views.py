2f032143134f66746c47407dac556be903460418
rhodlib <rhodlib@gmail.com>
2024-08-15 21:47:01 -0300

    search results view and searchbar

--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Book
 
 
-# Create your views here.
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
@@ -17,3 +17,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
